7f94c1cc4a542e631a42c520f7291aa2269aa7a8
ismailnawzat <54473074+ismailnawzat@users.noreply.github.com>
2019-09-12 00:23:31 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ client.on('message', message => {
   if (!message.content.startsWith(prefix)) return;
   var args = message.content.split(' ').slice(1);
   var argresult = args.join(' ');
-  if (message.author.id !== "608712003707731970") return;
+  if (message.author.id !== "574651726838366220") return;
 
   
   if (message.content.startsWith(prefix + 'setwatch')) {
